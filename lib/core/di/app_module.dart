651952557b179570ed497b6b7d2f162d832188ab
Lucas Didur <ldidur@icloud.com>
2023-08-11 12:20:26 -0300

    feat: implementacao versao do app rodapé

--- lib/core/di/app_module.dart
+++ lib/core/di/app_module.dart
@@ -1,8 +1,12 @@
 import 'package:injectable/injectable.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 @module
 abstract class AppModule {
   @lazySingleton
   InternetConnectionCheckerPlus get internetConnectionCheckerPlus => InternetConnectionCheckerPlus();
+
+  @preResolve
+  Future<PackageInfo> get packageInfo => PackageInfo.fromPlatform();
 }
